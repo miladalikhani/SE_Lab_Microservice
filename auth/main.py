32602d6300bd65fb4008af852e577777d5994ab1
m.chraghalikhani <miladalikhani76@gmail.com>
2021-07-01 01:48:11 +0430

    add signup without create profile

--- auth/main.py
+++ auth/main.py
@@ -26,6 +26,51 @@ def check_user(username, password):
     return False
 
 
+def user_exists(username):
+    auth_users = get_all_users()
+    for entity in auth_users['users']:
+        if entity['username'] == username:
+            return True
+    return False
+
+
+def create_auth_user(username, password):
+    try:
+        auth_users_file = open("auth_users.json", "r+")
+        auth_users = json.load(auth_users_file)
+        new_user = {}
+        new_user['username'] = username
+        new_user['password'] = password
+        auth_users['users'].append(new_user)
+        auth_users_file.seek(0)
+        json.dump(auth_users, auth_users_file)
+        auth_users_file.truncate()
+        auth_users_file.close()
+        return True
+    except:
+        return False
+
+
+def delete_auth_user(username,password):
+    try:
+        auth_users_file = open("auth_users.json", "r+")
+        auth_users = json.load(auth_users_file)
+        temp = {"username" : username, "password" : password}
+        flag = False
+        for entity in auth_users['users']:
+            if entity['username'] == username and entity['password'] == password:
+                flag = True
+        if flag:
+            auth_users['users'].remove(temp)
+        auth_users_file.seek(0)
+        json.dump(auth_users, auth_users_file)
+        auth_users_file.truncate()
+        auth_users_file.close()
+        return True
+    except:
+        return False
+
+
 @app.route("/auth/login", methods=['POST'])
 def login():
     if not request.json:
@@ -65,6 +110,27 @@ def authenticate():
     if 'username' not in data:
         abort(401)
     return jsonify({"username": data['username']}), 200
+
+
+@app.route('/auth/signup', methods=['POST'])
+def signup():
+    if not request.json:
+        abort(406)
+    user_data = request.json
+    if not user_data.get('username') or not user_data.get('password') \
+            or not user_data.get('first_name') or not user_data.get('last_name'):
+        abort(406)
+    username = user_data.get('username')
+    password = user_data.get('password')
+    first_name = user_data.get('first_name')
+    last_name = user_data.get('last_name')
+    if user_exists(username):
+        abort(409)
+    if (create_auth_user(username,password)):
+        # delete_auth_user(username,password)
+        return jsonify({})
+    else:
+        abort(500)
 
 
 if __name__ == '__main__':
